71c1bf1a0e881a8dd162de4812e860734e076553
Tanryberdi <tanryberdi@gmail.com>
2024-09-30 09:29:01 +0500

    database integration

---
+++ internal/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"time"
+)
+
+func ConnectToDB(connStr string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func SaveRate(db *sql.DB, ask, bid float64, timestamp time.Time) error {
+	_, err := db.Exec("INSERT INTO rates (ask, bid, timestamp) VALUES ($1, $2, $3)", ask, bid, timestamp)
+	return err
+}
